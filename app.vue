0a2c91c06f8c2389bc712b13ab0dff5b6cd2c1e4
GlennLanexCorp <glenn.pacantarra@lanexcorp.com>
2023-09-06 19:00:22 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <!--  not working for now<NuxtPage /> -->
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          </ul>
+        </nav>
+    </header>
+</template>
+
